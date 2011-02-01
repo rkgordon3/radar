270768a60f0cb01add671e86dcdfb97452403174
Chris <cmenge07@smumn.edu>
2011-02-01 00:40:41 -0600

    Trying to add some ruby files...

---
+++ hello/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = 'e386ea5b92bc2cb9deacd01d82f3a8824646867d2628352655769a30f362ca969fab6d894122c6cdf85ed9f0a69c83c3115570b0f34dd7b8dea7f845e6fa1300'
