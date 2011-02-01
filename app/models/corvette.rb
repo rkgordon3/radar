51c8e7e58644552ef5dff9741ddb9bfa1932b85f
Chris <cmenge07@smumn.edu>
2011-02-01 11:12:59 -0600

    Trying some new ruby files.

--- app/models/corvette.rb
+++
@@ -1,2 +0,0 @@
-class Corvette < ActiveRecord::Base
-end
--- db/migrate/20101214204455_add_state_to_corvette.rb
+++
@@ -1,9 +0,0 @@
-class AddStateToCorvette < ActiveRecord::Migration
-  def self.up
-    add_column :corvettes, :state, :string
-  end
-
-  def self.down
-    remove_column :corvettes, :state
-  end
-end
