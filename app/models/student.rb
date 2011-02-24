eeb79f57199809caae2424d3841d22f9909164e2
Emily Friedl <emfrie06@smumn.edu>
2011-02-24 11:32:10 -0600

    fixed merge conflicts

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,12 +1,10 @@
 class Student < Participant
 	belongs_to :building
 	
-<<<<<<< HEAD
 	def full_name
-		"#{self.first_name} #{self.last_name} #{self.building.abbreviation} #{self.room_number}"
-=======
+		"#{self.first_name} #{self.last_name} #{self.building.abbreviation} #{self.room_number} #{self.id}"
+		end
 	def get_id
 		"#{:id}"
->>>>>>> cf3dc711e0dd3ac7a8650072665dae68ea0afe1c
 	end
 end
