97149157962eb8e8a04faf1a3519d4be9d15d34a
Stephanie Valentine <dancing2bfree@gmail.com>
2011-02-28 17:47:22 -0600

    fixing chris's issues

--- test/functional/incident_report_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class IncidentReportControllerTest < ActionController::TestCase
-  test "should get incident_report" do
-    get :incident_report
-    assert_response :success
-  end
-
-end
