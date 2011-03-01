2a320e98be207a9700133c7395fcda4ce4fce9ca
Mitchell May <mjmay08@smumn.edu>
2011-03-01 11:35:32 -0600

    Query database for recently submitted reports rather than iterating through all of them.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
 	
   def landingpage
   @reports = Report.all
+  
+  @recent = Report.where("created_at > ?", current_staff.last_sign_in_at)
   end
 
 end
