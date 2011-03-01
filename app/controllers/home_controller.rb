5f1d75cca905048f1a7fe4407f4cb80291ae2453
Brad Wildenborg <bwildenborg@gmail.com>
2011-03-01 11:38:13 -0600

    Modified unsubmitted report view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
 	
   def landingpage
   @reports = Report.all
+  if staff_signed_in?
+	@unsubmitted = Report.where("submitted = ? AND staff_id = ?", false, current_staff.id)
+  end
   end
 
 end
