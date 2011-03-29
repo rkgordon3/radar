d5592ba32a1c5ce046dbbf30ac5b5361ff6ac10c
Emily Friedl <emfrie06@smumn.edu>
2011-03-28 20:06:56 -0500

    thumbnails working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,18 @@
 class SearchController < ApplicationController
   before_filter :general_authorize  
-  # autocomplete looks at the student table in the full name field, returns all values that match
   autocomplete :student, :full_name, :display_value => :full_name, :full => true
   
- 
+  def search
+  end
+  
+  def get_autocomplete_items(parameters)
+    Student.where("first_name LIKE ? OR last_name LIKE ?", "#{parameters[:term]}%").order(:first_name).order(:last_name)
+  end
+  
   
   def go_to_student
-	#returns student that was selected from autocomplete
     @student = Student.get_student_object_for_string(params[:full_name])
     
-	#redirects to the students page
     respond_to do |format|
       format.html { redirect_to "/students/" + @student.id.to_s }
       format.xml  { render :xml => @student}
@@ -19,93 +22,44 @@ class SearchController < ApplicationController
   end
   
   
-  
-  def delete_student
-  #take the params[:id] to get the index to delete the student out of the session and replace the message
-    #if there are no students currently in the session
-    if session[:students] == nil
-	  # create new array
-      session[:students] = Array.new
-    end
-	
-	#assigns all students in the session(0..n) to @students
-    @students = session[:students]
-	student_to_delete = nil
-	@students.each do |s|
-	  #check each student's id in the array agains the student to delete
-      if params[s.id.to_s] != nil
-		student_to_delete = s
-		logger.debug "The object is #{s.first_name}"
-		end
-    end
-	
-	if student_to_delete != nil
-		
-		@students.delete(student_to_delete)
-	end
-	#initialize message
-    
-    
-	@message = self.message(@students)
-	
-    
-	#replace 'found' (in report_search) with the new message
-    render :update do |page|
-      page.replace_html 'found', @message
-    end
-  
-  end
-  
-  
-  def message(students)
-	@message = ''
-    students.each do |s|
-	  #message will be all students first and last names
-      #@message = @message + '<p>' + '<input type="submit" value="X" name="' + s.id.to_s + '"> </input>' + s.first_name + ' ' + s.last_name + '</p>'
-	  @message = @message + '<p>' + '<input id="' + s.id.to_s + '" name="' + s.id.to_s + '" type="submit" value="X" />' + s.first_name + ' ' + s.last_name + '</p>'
-	  end
-	return @message
-  end
-  
-  
-  
   def update_list
-    #returns student that was selected from autocomplete
+    
     @student = Student.get_student_object_for_string(params[:full_name])
     
-	#if there are no students currently in the session
     if session[:students] == nil
-	  # create new array
       session[:students] = Array.new
     end
-	
-	#assigns all students in the session(0..n) to @students
     @students = session[:students]
     
-	#check to make sure a student doesn't already exist in the array of students in the session
     exists = false
-	#for each student
     @students.each do |s|
-	  #check each student's id in the array agains the student to add
       if s.id == @student.id
         exists = true
       end
     end
     
-	#if the student was not found in the students in the session
     if exists == false
-	  #add the student selected to the students in the session
       @students << @student
     end
     
-	#initialize message
-    @message = self.message(@students)
+    @message = ''
     
-	#replace 'found' (in report_search) with the new message
+    @students.each do |s|
+      @message = @message + '<p>' + '<img src="' + self.getURLforID(s.id.to_s) + '"/>' + ' ' + s.first_name + ' ' + s.last_name + '</p>'
+    end
+    
+    
     render :update do |page|
       page.replace_html 'found', @message
     end
     
   end
+  
+  
+  def getURLforID(id)
+    url = Student.find(id).first_name.downcase + "." + Student.find(id).last_name.downcase
+    image_url = STUDENT_THUMBS_PATH + url
+  end
+    
 
 end
