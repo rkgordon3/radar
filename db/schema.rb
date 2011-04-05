a1a4d753e5cdc8b1030d2e91120d6454fcca7bf7
Emily Friedl <emfrie06@smumn.edu>
2011-04-04 21:09:57 -0500

    db changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110330002836) do
+ActiveRecord::Schema.define(:version => 20110403221557) do
 
   create_table "annotations", :force => true do |t|
     t.datetime "created_at"
@@ -93,6 +93,8 @@ ActiveRecord::Schema.define(:version => 20110330002836) do
     t.integer  "staff_id"
     t.boolean  "submitted"
     t.integer  "annotation_id"
+    t.string   "fieldname"
+    t.string   "report_no"
   end
 
   create_table "sessions", :force => true do |t|
