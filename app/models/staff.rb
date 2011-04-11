0dcd70a985afac0d9480525f3a196a0f6441442d
Mitchell May <mjmay08@smumn.edu>
2011-04-10 19:38:51 -0500

    Login page email is now case insensitive.

--- app/models/staff.rb
+++ app/models/staff.rb
@@ -1,5 +1,9 @@
 class Staff < ActiveRecord::Base
 	has_many :notification_preferences
+    before_save :lower_email
+    def lower_email
+      self.email = email.downcase
+    end
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
