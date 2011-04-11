15aa45651fc9ccc615d32bb70a40f0707afaf9ed
Chris <cmenge07@smumn.edu>
2011-04-10 19:24:43 -0500

    Immediate Notification for IR

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,26 @@
 class Notification
-	def immediate_notify(id => report.id)
-		report = report.where(:id => id)
+	def Notification.immediate_notify(id)
+		notify_prefs = Array.new
+		
+		#staff_list = Array.new
+		
+		report = Report.find(id)
+		
+		notify_prefs = NotificationPreference.where(:frequency => 1, :report_type => report.type).all
+		
+		notify_prefs.each do |np|
+			
+			mail = RadarMailer.immediate_notification_mail(report, Staff.find(np.staff_id))
+			mail.deliver
+			
+			#s = Staff.find(np.staff_id)
+			#staff_list << s
+			
+		end	
+		
+		#staff_list.each do |staff|
+		#	mail = RadarMailer.immediate_notification_mail(report, staff)
+		#	mail.deliver
+		#end	
 	end
-
 end
