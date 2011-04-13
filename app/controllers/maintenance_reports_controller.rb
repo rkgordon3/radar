1b1c29a24f9c3fe91fb465f0ceba7f49bf5b5ba3
rgordon <rgordon@smumn.edu>
2011-04-13 13:45:18 -0500

    created show_relationships for partial to be used to show participants in report (used by views/*/show.html.erb); show_relationships takes a div so that title of relationships is parameterized wrapped some headers in div for future css work created index/show/edit actions/views for maintenance requests incident_report show now uses show_generic/show_relationships

--- app/controllers/maintenance_reports_controller.rb
+++ app/controllers/maintenance_reports_controller.rb
@@ -7,4 +7,24 @@ class MaintenanceReportsController < ReportsController
 												format.html
 								end
 				end
+				
+				 # GET /incident_reports
+  # GET /incident_reports.xml
+  def index
+
+  	@reports = MaintenanceReport.where(:submitted => true).order(:approach_time)
+
+    
+    respond_to do |format|
+    	format.html { render :locals => { :reports => @reports } }
+      format.xml  { render :xml => @reports }
+      format.iphone {render :layout => 'mobile_application'}
+    end
+  end
+  
+  def edit
+  				respond_to do |format|
+  								format.html { render :text => "Edit of Maintenance Requests not supported. Shucks." }
+  				end
+  end
 end
