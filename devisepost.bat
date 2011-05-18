6aacd4d485f4d559aeb8251a17daf99ab198e947
Rob Gordon <rgordon@smumn.edu>
2011-05-18 10:54:42 -0500

    modified message s.t. example of devise_dir is provided

--- devisepost.bat
+++ devisepost.bat
@@ -1,11 +1,11 @@
 IF "%devise_dir%"=="" GOTO END
-copy "devise views\sessions_controller.rb" "%devise_dir%app\controllers\devise\sessions_controller.rb"
-copy "devise views\new.iphone.erb" "%devise_dir%app\views\devise\sessions\new.iphone.erb"
-copy "devise views\registrations_controller.rb" "%devise_dir%app\controllers\devise\registrations_controller.rb"
-copy "devise views\new.html.erb" "%devise_dir%app\views\devise\registrations\new.html.erb"
-copy "devise views\_links.erb" "%devise_dir%app\views\devise\shared\_links.erb"
+copy "devise views\sessions_controller.rb" "%devise_dir%\app\controllers\devise\sessions_controller.rb"
+copy "devise views\new.iphone.erb" "%devise_dir%\app\views\devise\sessions\new.iphone.erb"
+copy "devise views\registrations_controller.rb" "%devise_dir%\app\controllers\devise\registrations_controller.rb"
+copy "devise views\new.html.erb" "%devise_dir%\app\views\devise\registrations\new.html.erb"
+copy "devise views\_links.erb" "%devise_dir%\app\views\devise\shared\_links.erb"
 GOTO END2
 :END
-ECHO "Devise Dir Not Found"
+ECHO "devise_dir environment variable must be set, e.g.c:\Ruby192\lib\ruby\gems\1.9.1\gems\devise-1.1.7" "
 :END2
 
--- devisepre.bat
+++ devisepre.bat
@@ -1,10 +1,10 @@
 IF "%devise_dir%"=="" GOTO END
-copy "%devise_dir%app\controllers\devise\sessions_controller.rb" "devise views\sessions_controller.rb"
-copy "%devise_dir%app\views\devise\sessions\new.iphone.erb" "devise views\new.iphone.erb"
-copy "%devise_dir%app\controllers\devise\registrations_controller.rb" "devise views\registrations_controller.rb"
-copy "%devise_dir%app\views\devise\registrations\new.html.erb" "devise views\new.html.erb"
-copy "%devise_dir%app\views\devise\shared\_links.erb" "devise views\_links.erb"
+copy "%devise_dir%\app\controllers\devise\sessions_controller.rb" "devise views\sessions_controller.rb"
+copy "%devise_dir%\app\views\devise\sessions\new.iphone.erb" "devise views\new.iphone.erb"
+copy "%devise_dir%\app\controllers\devise\registrations_controller.rb" "devise views\registrations_controller.rb"
+copy "%devise_dir%\app\views\devise\registrations\new.html.erb" "devise views\new.html.erb"
+copy "%devise_dir%\app\views\devise\shared\_links.erb" "devise views\_links.erb"
 GOTO END2
 :END
-ECHO "Devise Dir Not Found"
+ECHO "devise_dir environment variable must be set, e.g. c:\Ruby192\lib\ruby\gems\1.9.1\gems\devise-1.1.7"
 :END2
