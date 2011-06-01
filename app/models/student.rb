85129f6f75914de176705792f726b6e5b81bae43
Lyric Hupp <llhupp09@smumn.edu>
2011-06-01 15:43:47 -0500

    ( https://www.pivotaltracker.com/story/show/13605607 )
    - Added middle_initial to Participant model
    - Modified all views which displayed first_name and last_name to now also display middle_initial

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,7 +4,7 @@ class Student < Participant
 	#self.view_name = "participant_view" 
 	
 	def full_name
-		"#{self.first_name} #{self.last_name}, #{self.building.abbreviation}, #{self.room_number}"
+		"#{self.first_name} #{self.middle_initial} #{self.last_name}, #{self.building.abbreviation}, #{self.room_number}"
 		end
 	def get_id
 		"#{:id}"
