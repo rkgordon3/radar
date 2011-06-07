573922f42ce919a342ba1f3d6aa5a953b48af1ad
Lyric Hupp <llhupp09@smumn.edu>
2011-06-07 16:28:25 -0500

    ( https://www.pivotaltracker.com/story/show/14293265 ) - added tasks to duty logs - modified rounds in duty logs to be togglified

--- app/models/task_assignment.rb
+++ app/models/task_assignment.rb
@@ -2,4 +2,13 @@ class TaskAssignment < ActiveRecord::Base
   belongs_to :shift
   belongs_to :task
   
+  def done_string
+    if self.done
+      return "yes"
+    else
+      return "no"
+    end
+    
+  end
+  
 end
