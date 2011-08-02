a578816cd0ed7b6da91db4cb701b0664d4818765
Lyric Hupp <llhupp09@smumn.edu>
2011-08-02 15:59:47 -0500

    - Added report type display to top of iphone views

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,6 +2,7 @@ class NotesController < ReportsController
   load_and_authorize_resource
   
 	def new
+    #TODO: should be done by reports_controller and rendered by reports views
 		@report = Note.new(:staff_id => current_staff.id)
                 @report.process_participant_params_string_from_student_search(params[:participants])
                  session[:report] = @report
