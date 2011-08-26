85cc0fb7b65632c59b729ce73bb59b1897a6a1b5
Lyric Hupp <llhupp09@smumn.edu>
2011-08-26 14:12:47 -0500

    - removed RA ability to print or forward reports

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,7 @@ class Ability
   def initialize(staff)
     alias_action :start_shift, :end_shift, :update_todo, :start_round, :end_round, :update, :to => :do
     alias_action :call_log, :duty_log, :to => :shift_log
+    alias_action :forward_as_mail => :forward
 
     staff ||= Staff.new
 
@@ -84,6 +85,7 @@ class Ability
     elsif access_level_symbol == :resident_assistant
       cannot [:read, :update, :destroy], [Staff, Report]
       can [:read, :update], Report, :staff_id => staff.id
+      cannot [:print, :forward], Report
       cannot :update, Report, :submitted => true
 
       cannot :manage, Shift
