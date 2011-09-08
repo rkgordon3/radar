39237d1d4ec9eda61d60263c217ec51b18f19218
Rob Gordon <rgordon@smumn.edu>
2011-09-08 16:50:56 -0500

    fixes to scripts for update/drop

--- script/drop_students.rb
+++ script/drop_students.rb
@@ -1,10 +1,11 @@
-load 'ar_support.rb'
+$LOAD_PATH << "."
+require "ar_support"
 
 lines = ImportsHelper.parse_csv_file(File.expand_path("./shradarwdraw.csv"))
 lines.shift
 print "Process #{lines.size} drop records"
 scnt = ImportsHelper.deactivate_students(lines)
-puts "Successfully dropped #{scnt} students. #{lines.size-scnt} failures."
+puts "Successfully processed #{scnt} students. #{lines.size-scnt} failures."
 puts ImportsHelper::Helpers.stats
 puts "ERROR MESSAGES:"
 puts ImportsHelper::Helpers.error_messages
