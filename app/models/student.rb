85aca1764271440e55e5ac7de28f4b85b1b64604
Rob Gordon <suffolksoft@gmail.com>
2011-12-02 16:50:06 -0600

    removed get_id

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,14 +1,9 @@
 class Student < Participant
 	
-	#self.view_name = "participant_view" 
-	
 	def full_name
 		"#{self.first_name} #{self.middle_initial} #{self.last_name}, #{self.building.abbreviation}, #{self.room_number}"
-  end
-  
-	def get_id
-		"#{:id}"
-	end
+    end
+ 
 	
 	def residence_hall
 	  building.name rescue unspecified
