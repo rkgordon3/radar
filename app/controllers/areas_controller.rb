dc76d76f2d07db3dfdcabc6b7a9cc37c6bdcb8e6
Lyric <llhupp09@smumn.edu>
2011-12-15 23:12:27 -0600

    - Added sorting functionality to "Manage Areas" page - No reload of "Manage Areas" page on "create"

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -6,10 +6,9 @@ class AreasController < ApplicationController
 
   def index
     sort = params[:sort]
-    @areas = @areas.where("id <> ?", Area.unspecified_id)
-    #@areas = Area.sort(sort)
-    msg = "Buildings are now sorted by #{sort}."
-    unassigned_buildings = Building.where( :area_id => Area.unspecified_id )
+    @areas = Area.sort(sort)
+    msg = "Areas are now sorted by #{sort}."
+    unassigned_buildings = Building.where( :area_id => Area.unspecified_id ).order("name ASC")
 
     respond_to do |format|
       format.html { render :locals => { :unassigned_buildings => unassigned_buildings } }
@@ -20,10 +19,19 @@ class AreasController < ApplicationController
   # POST /buildings
   # POST /buildings.xml
   def create
-    @area.buildings = Building.find(params[:buildings])
-    authorize! :create, @area
-    @areas = Area.accessible_by(current_ability)
-    @areas = @areas.where("id <> ?", Area.unspecified_id)
+    is_shaded = params[:is][:shaded]
+    if is_shaded == "1"
+      row_style = "class='shaded'"
+      is_shaded = 0
+    else
+      row_style = ""
+      is_shaded = 1
+    end
+
+    if params[:buildings] != nil
+      @area.buildings = Building.find(params[:buildings])
+      authorize! :create, @area
+    end
 
     respond_to do |format|
       if @area.save
@@ -31,26 +39,24 @@ class AreasController < ApplicationController
       else
         msg = "Error: Area NOT created!"
       end
-      unassigned_buildings = Building.where( :area_id => Area.unspecified_id )
-      format.js { render "areas/index", :locals => { :unassigned_buildings => unassigned_buildings, :flash_notice => msg } }
+      unassigned_buildings = Building.where( :area_id => Area.unspecified_id ).order("name ASC")
+      format.js { render :locals => { :row_style => row_style, :is_shaded => is_shaded, :unassigned_buildings => unassigned_buildings, :flash_notice => msg } }
     end
   end
 
   # PUT /buildings/1
   # PUT /buildings/1.xml
   def update
-    buildings_to_be_assigned = Building.find(params[:buildings])
-    @area.buildings =  buildings_to_be_assigned
-    authorize! :update, @area
-    @areas = Area.accessible_by(current_ability)
-    @areas = @areas.where("id <> ?", Area.unspecified_id)
+    params[:area][:buildings] = params[:buildings]
+    @areas = Area.sort("name")
+
     respond_to do |format|
       if @area.update_attributes(params[:area])
         msg = "#{@area.name} has been successfully updated."
       else
         msg = "Error: Area NOT updated!"
       end
-      unassigned_buildings = Building.where( :area_id => Area.unspecified_id )
+      unassigned_buildings = Building.where( :area_id => Area.unspecified_id ).order("name ASC")
       format.js { render "areas/index", :locals => { :unassigned_buildings => unassigned_buildings, :flash_notice => msg } }
     end
   end
@@ -59,11 +65,10 @@ class AreasController < ApplicationController
   # DELETE /buildings/1.xml
   def destroy
     msg = "#{@area.name} has been successfully destroyed."
-    id = @area.id
     @area.destroy
-    unassigned_buildings = Building.where( :area_id => Area.unspecified_id )
-    @areas = Area.accessible_by(current_ability)
-    @areas = @areas.where("id <> ?", Area.unspecified_id)
+    unassigned_buildings = Building.where( :area_id => Area.unspecified_id ).order("name ASC")
+    @areas = Area.sort("name")
+    
     respond_to do |format|
       format.js { render "areas/index", :locals => { :unassigned_buildings => unassigned_buildings, :flash_notice => msg } }
     end
--- app/models/area.rb
+++ app/models/area.rb
@@ -6,6 +6,19 @@ class Area < ActiveRecord::Base
     Area.find_by_name(unspecified).id rescue 0
   end
 
+  def Area.sort(key)
+    a = Area.where("id <> ?", Area.unspecified_id)
+    if key=="name"
+      return a.order("name ASC").all
+    elsif key=="abbreviation"
+      return a.order("abbreviation ASC").all
+    elsif key=="buildings"
+      return a.order("name ASC").all
+    else
+      return a.order("name ASC").all
+    end
+  end
+
   def destroy
     buildings = Building.where(:area_id => self.id)
     buildings.each do |b|
@@ -14,8 +27,17 @@ class Area < ActiveRecord::Base
     end
     super
   end
+
+  def update_attributes(params)
+    buildings = Building.where(:area_id => self.id)
+    buildings.each do |b|
+      b.area_id = Area.unspecified_id
+      b.save
+    end
+    self.buildings = Building.find(params[:buildings]) if params[:buildings] != nil
+    super(params)
+  end
   
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name,:abbreviation
-  
+  attr_accessible :name,:abbreviation  
 end
