a6c87ed71a8ff83eae97bf5b74ddeb9130981d4b
Rob Gordon <suffolksoft@gmail.com>
2011-12-30 13:29:27 -0600

    fixed (incorrect) removal of belongs_to, instead removed has_one annotation

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,11 +4,11 @@ class Report < ActiveRecord::Base
   belongs_to		:organization
   has_many      	:forwards, :foreign_key => :report_id, :class_name => "InterestedPartyReport"
   has_many     		:adjunct_submitters, :foreign_key => :report_id, :class_name => "ReportAdjunct"
-  has_one		    :annotation
+  #has_one		    :annotation
   has_many      	:report_participant_relationships
   has_many      	:participants, :through => :report_participant_relationships
   
-  #belongs_to    	:annotation
+  belongs_to    	:annotation
   after_initialize 	:setup_defaults
   after_find		:cache_submitted
   after_save       	:save_everything
