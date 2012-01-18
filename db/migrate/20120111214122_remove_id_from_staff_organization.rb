20a36c04166b9f21f18d5d330b548cdc3ac69a0b
Lyric <llhupp09@smumn.edu>
2012-01-18 17:55:29 -0600

    - removed a migration to staff_orgs table - modified ftp_students.bat

--- db/migrate/20120111214122_remove_id_from_staff_organization.rb
+++
@@ -1,9 +0,0 @@
-class RemoveIdFromStaffOrganization < ActiveRecord::Migration
-  def self.up
-   remove_column :staff_organizations, :id
-  end
-
-  def self.down
-    add_column :staff_organizations, :id, :integer, :primary_key => true
-  end
-end
