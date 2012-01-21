df3d81c27052201a2d96c66a39a84fda926e724e
Lyric <llhupp09@smumn.edu>
2012-01-20 23:56:43 -0600

    - Modified ResLife and ASC abilities to only give "read" ability of ReportParticipantRelations of MY_REPORT_TYPES as defined in each respective class. +++Only gave this ability to access_levels which already had ability to "view_contact_history"
    - Modified "contact_history" method in participant.rb to utilize "ability" argument
    
    - added "current_ability" argument to all usages in various views

--- app/models/academic_skills_center_organization.rb
+++ app/models/academic_skills_center_organization.rb
@@ -6,49 +6,51 @@ class AcademicSkillsCenterOrganization < Organization
   def system_administrator(ability, staff)
   	ability.can :assign, AccessLevel, :name => ["Administrator", "AdministrativeAssistant", "Supervisor", "Staff"]
     ability.can [:index, :search, :view_contact_info, :view_contact_history, :show], Participant
-	ability.can :search, MY_REPORTS
-	ability.can [:select], ReportType, { :name => MY_REPORT_TYPES }
-	# Can register users in this organization
-	ability.can :register, Organization, :id => self.id	  
-	# Limit access to those reports in this organization
-	ability.can [:create, :read, :show, :index, :update, :search, :add_participant, :remove_participant, :create_participant_and_add_to_report], Report, :organization_id => self.id
+    ability.can :search, MY_REPORTS
+    ability.can :read, ReportParticipantRelationship, { :report => {:type => MY_REPORT_TYPES} }
+    ability.can [:select], ReportType, { :name => MY_REPORT_TYPES }
+    # Can register users in this organization
+    ability.can :register, Organization, :id => self.id
+    # Limit access to those reports in this organization
+    ability.can [:create, :read, :show, :index, :update, :search, :add_participant, :remove_participant, :create_participant_and_add_to_report], Report, :organization_id => self.id
 
-	ability.can [:read], Staff
-	ability.can :update, Staff, :id => staff.id
-	ability.can [:create, :update, :destroy], Staff, 
-                 	{ 
-					  :organizations => { :id => self.id },
-					  :access_levels  => {:display_name => ["Administrator", "Administrative Assistant", "Supervisor", "Staff"]}
-					} 
-	ability.can [:select], ReportType, { :name => MY_REPORT_TYPES }
+    ability.can [:read], Staff
+    ability.can :update, Staff, :id => staff.id
+    ability.can [:create, :update, :destroy], Staff,
+      {
+      :organizations => { :id => self.id },
+      :access_levels  => {:display_name => ["Administrator", "Administrative Assistant", "Supervisor", "Staff"]}
+    }
+    ability.can [:select], ReportType, { :name => MY_REPORT_TYPES }
   end
     
   def supervisor(ability, staff)
-	ability.can [:create, :search, :read], Report, { :type => MY_REPORT_TYPES  }
-	ability.can [:index, :search, :view_contact_info, :view_contact_history, :show], Participant
-	ability.can [:index], Report, { :type => ["TutorReport" , "TutorByAppointmentReport" , "Note" ]  }
-	ability.can [:search, :read, :create, :update, :add_participant], MY_REPORTS, :staff_id => staff.id 	  
-	# Can index staff within my organization
+    ability.can [:create, :search, :read], Report, { :type => MY_REPORT_TYPES  }
+    ability.can [:index, :search, :view_contact_info, :view_contact_history, :show], Participant
+    ability.can :read, ReportParticipantRelationship, { :report => {:type => MY_REPORT_TYPES} }
+    ability.can [:index], Report, { :type => ["TutorReport" , "TutorByAppointmentReport" , "Note" ]  }
+    ability.can [:search, :read, :create, :update, :add_participant], MY_REPORTS, :staff_id => staff.id
+    # Can index staff within my organization
     ability.can :index, Staff, :organizations => { :id => self.id }
-	ability.can [:update, :show], Staff, :id => staff.id    
+    ability.can [:update, :show], Staff, :id => staff.id
     ability.can [:select], ReportType, { :name => MY_REPORT_TYPES }
   end
   
   def staff(ability, staff)
-      puts ( "*********Apply abilities to staff")
-	ability.can [:index, :search], Participant
+    puts ( "*********Apply abilities to staff")
+    ability.can [:index, :search], Participant
 
 
     ability.can [:create, :search, ], Report, { :type => MY_REPORT_TYPES }
-	ability.can [:create, :search, :update,:index], Note
-	ability.can [:index, :search, :read, :add_participant, :remove_participant, :create_participant_and_add_to_report], 
-	             Report,
-				{ :type => MY_REPORT_TYPES, :staff_id => staff.id } 
+    ability.can [:create, :search, :update,:index], Note
+    ability.can [:index, :search, :read, :add_participant, :remove_participant, :create_participant_and_add_to_report],
+      Report,
+      { :type => MY_REPORT_TYPES, :staff_id => staff.id }
     ability.can [:update], Report, { :staff_id => staff.id,  :type => MY_REPORT_TYPES, :submitted => false }
-	ability.can [:index, :update], TutorReport, { :staff_id => staff.id  }
-	# Can index staff within my organization
+    ability.can [:index, :update], TutorReport, { :staff_id => staff.id  }
+    # Can index staff within my organization
     ability.can :index, Staff, :organizations => { :id => self.id }
-	ability.can [:update, :show], Staff, :id => staff.id
+    ability.can [:update, :show], Staff, :id => staff.id
 
 
     ability.can [:select], ReportType, { :name => MY_REPORT_TYPES }
--- app/models/residence_life_organization.rb
+++ app/models/residence_life_organization.rb
@@ -6,82 +6,85 @@ class ResidenceLifeOrganization < Organization
 
   def system_administrator(ability, staff) 
     ability.can [:index, :search, :view_contact_info, :view_contact_history, :show], Participant
-	ability.can [:create, :read, :update, :search, :pdf, :show], MY_REPORTS
-	# Limit access to those reports in this organization
+    ability.can [:create, :read, :update, :search, :pdf, :show], MY_REPORTS
+    ability.can :read, ReportParticipantRelationship, { :report => {:type => MY_REPORT_TYPES} }
+    # Limit access to those reports in this organization
     ability.can [:create, :read, :show, :index, :update, :search, :add_participant, :remove_participant, :create_participant_and_add_to_report], Report, :organization_id => self.id
-	# Can view all staff 
-	ability.can [:read], Staff
-	# These access_levels are being deprecated. Staff => Resident Assistant, Supervisor => HD
-	ability.can [:create, :update, :destroy, :update_access_level ], Staff, 
-					{
-						:organizations => { :id => self.id },
-						:access_levels=> {:name => ["HallDirector", "ResidentAssistant", "CampusSafety" ]}
-					}
-	# These access levels are organization-generic.
-	ability.can [:create, :update, :destroy, :update_access_level], Staff, 
-					{ 
-						:organizations => { :id => self.id },
-						:access_levels => {:name => ["Administrator", "Administrative Assistant", "Supervisor", "Staff"]}
-					}
-	ability.can :manage, [Task, Building, Area]
-	ability.can :manage, NotificationPreference
-	ability.can :register, Organization, :id => self.id
-	ability.can :assign, AccessLevel, :name => ["Administrator", "AdministrativeAssistant", "Supervisor", "Staff", "CampusSafety", "HallDirector", "ResidentAssistant"]
-	ability.can :assign, Area
-	ability.can [:select], ReportType, { :name => MY_REPORT_TYPES }
+    # Can view all staff
+    ability.can [:read], Staff
+    # These access_levels are being deprecated. Staff => Resident Assistant, Supervisor => HD
+    ability.can [:create, :update, :destroy, :update_access_level ], Staff,
+      {
+      :organizations => { :id => self.id },
+      :access_levels=> {:name => ["HallDirector", "ResidentAssistant", "CampusSafety" ]}
+    }
+    # These access levels are organization-generic.
+    ability.can [:create, :update, :destroy, :update_access_level], Staff,
+      {
+      :organizations => { :id => self.id },
+      :access_levels => {:name => ["Administrator", "Administrative Assistant", "Supervisor", "Staff"]}
+    }
+    ability.can :manage, [Task, Building, Area]
+    ability.can :manage, NotificationPreference
+    ability.can :register, Organization, :id => self.id
+    ability.can :assign, AccessLevel, :name => ["Administrator", "AdministrativeAssistant", "Supervisor", "Staff", "CampusSafety", "HallDirector", "ResidentAssistant"]
+    ability.can :assign, Area
+    ability.can [:select], ReportType, { :name => MY_REPORT_TYPES }
   end
   
   def administrator(ability, staff)
-	ability.can :assign, AccessLevel, :name => ["AdministrativeAssistant", "Supervisor", "Staff", "CampusSafety"]
+    ability.can :assign, AccessLevel, :name => ["AdministrativeAssistant", "Supervisor", "Staff", "CampusSafety"]
   	ability.can :register, Organization, :id => self.id
     ability.can [:index, :search, :view_contact_info, :view_contact_history, :show], Participant
-	ability.can [:create, :read, :update, :search, :show], MY_REPORTS
+    ability.can :read, ReportParticipantRelationship, { :report => {:type => MY_REPORT_TYPES} }
+    ability.can [:create, :read, :update, :search, :show], MY_REPORTS
     ability.can [:add_participant, :create_participant_and_add_to_report, :remove_participant], Report, { :type => MY_REPORT_TYPES}
-	ability.can [:create, :update, :read, :pdf], Report, { :type => MY_REPORT_TYPES }
-	# Can view staff in my org
-	ability.can :index, Staff, organizations => { :id => self.id }
-	# Can c/u/d HD and RA in my org. These levels to be deprecated.
-	ability.can [:create, :update, :destroy], Staff, 
-					{
-						:organizations => { :id => self.id },
-						:access_levels => {:name => ["HallDirector", "ResidentAssistant", "CampusSafety"]}
-					}
-	# Can c/u/d admin asst, supervisor and staff for my org.
-	ability.can [:create, :update, :destroy], Staff, 
-					{
-						:organizations => { :id => self.id },
-						:access_levels => {:name => ["AdministrativeAssistant", "Supervisor", "Staff"]}
-					}
-	ability.can :update, NotificationPreference
-	ability.can :manage, Task
-	ability.can [:register, :update], Staff, :organizations => { :id => self.id }	
-	ability.can :assign, Area
-	ability.can [:select], ReportType, { :name => MY_REPORT_TYPES }
+    ability.can [:create, :update, :read, :pdf], Report, { :type => MY_REPORT_TYPES }
+    # Can view staff in my org
+    ability.can :index, Staff, organizations => { :id => self.id }
+    # Can c/u/d HD and RA in my org. These levels to be deprecated.
+    ability.can [:create, :update, :destroy], Staff,
+      {
+      :organizations => { :id => self.id },
+      :access_levels => {:name => ["HallDirector", "ResidentAssistant", "CampusSafety"]}
+    }
+    # Can c/u/d admin asst, supervisor and staff for my org.
+    ability.can [:create, :update, :destroy], Staff,
+      {
+      :organizations => { :id => self.id },
+      :access_levels => {:name => ["AdministrativeAssistant", "Supervisor", "Staff"]}
+    }
+    ability.can :update, NotificationPreference
+    ability.can :manage, Task
+    ability.can [:register, :update], Staff, :organizations => { :id => self.id }
+    ability.can :assign, Area
+    ability.can [:select], ReportType, { :name => MY_REPORT_TYPES }
   end
   
   def administrative_assistant(ability, staff)
     ability.can [:index, :search, :view_contact_info, :view_contact_history, :show], Participant
-	ability.can [:add_participant, :create_participant_and_add_to_report, :remove_participant], Report, { :staff_id => staff.id }
-	ability.can [:search] , MY_REPORTS
-	ability.can [:create, :pdf, :show], Report, { :type => MY_REPORT_TYPES }
+    ability.can :read, ReportParticipantRelationship, { :report => {:type => MY_REPORT_TYPES} }
+    ability.can [:add_participant, :create_participant_and_add_to_report, :remove_participant], Report, { :staff_id => staff.id }
+    ability.can [:search] , MY_REPORTS
+    ability.can [:create, :pdf, :show], Report, { :type => MY_REPORT_TYPES }
     ability.can [:read, :update], MY_REPORTS,  { :staff_id => staff.id, :submitted => false }
-	ability.can :update, NotificationPreference
-	ability.can :index, Staff, :organizations => { :id => self.id }
-	ability.can :manage, Task
-	ability.can [:select], ReportType, { :name => MY_REPORT_TYPES }
-	ability.can :search, Report
+    ability.can :update, NotificationPreference
+    ability.can :index, Staff, :organizations => { :id => self.id }
+    ability.can :manage, Task
+    ability.can [:select], ReportType, { :name => MY_REPORT_TYPES }
+    ability.can :search, Report
   end
   
   def campus_safety(ability, staff)
     ability.can [:index, :search, :view_contact_info, :show], Participant
-	ability.can [:create, :index, :search, :pdf, :show], Report, { :type => "IncidentReport" }
-	ability.can [:add_participant, :create_participant_and_add_to_report, :remove_participant], Report, { :staff_id => staff.id }
-	ability.can [:show], Note, {:staff_id => staff.id }
-	ability.can [:show], IncidentReport, { :staff_id => staff.id, :submitted  => false }
-	ability.can [ :update], IncidentReport, { :staff_id => staff.id, :submitted  => false, :type => "IncidentReport" }
-	ability.can :update, NotificationPreference, :staff_id => staff.id
-	ability.can :index, Staff, :organizations => { :id => self.id }
-	ability.can [:update, :show], Staff, :id => staff.id
+    ability.can [:create, :index, :search, :pdf, :show], Report, { :type => "IncidentReport" }
+    ability.can [:add_participant, :create_participant_and_add_to_report, :remove_participant], Report, { :staff_id => staff.id }
+    ability.can [:show], Note, {:staff_id => staff.id }
+    ability.can [:show], IncidentReport, { :staff_id => staff.id, :submitted  => false }
+    ability.can [ :update], IncidentReport, { :staff_id => staff.id, :submitted  => false, :type => "IncidentReport" }
+    ability.can :update, NotificationPreference, :staff_id => staff.id
+    ability.can :index, Staff, :organizations => { :id => self.id }
+    ability.can [:update, :show], Staff, :id => staff.id
 
   end
   
@@ -91,26 +94,27 @@ class ResidenceLifeOrganization < Organization
   
   def hall_director(ability, staff)
     puts ("*********Apply abilities to hall director")
-	ability.can [:index, :search, :view_contact_info, :view_contact_history, :show], Participant
-	ability.can [:create, :pdf], Report, { :type => MY_REPORT_TYPES}
+    ability.can [:index, :search, :view_contact_info, :view_contact_history, :show], Participant
+    ability.can :read, ReportParticipantRelationship, { :report => {:type => MY_REPORT_TYPES} }
+    ability.can [:create, :pdf], Report, { :type => MY_REPORT_TYPES}
     ability.can [:add_participant, :create_participant_and_add_to_report, :remove_participant], Report, {  :submitted  => false, :staff_id => staff.id }
-	ability.can [:show], [MaintenanceReport, Note], {:staff_id => staff.id }
-	ability.can [:show], IncidentReport, { :staff_id => staff.id, :submitted  => false }
+    ability.can [:show], [MaintenanceReport, Note], {:staff_id => staff.id }
+    ability.can [:show], IncidentReport, { :staff_id => staff.id, :submitted  => false }
     ability.can [:read, :update], MY_REPORTS,  { :staff_id => staff.id, :submitted => false }
-	ability.can :show, MY_REPORTS
-	#ability.can [:create, :read, :update], DutyLog
+    ability.can :show, MY_REPORTS
+    #ability.can [:create, :read, :update], DutyLog
     ability.can [:list_RA_duty_logs], Shift
     ability.can [:shift_log, :read], Shift, :staff => {:access_levels => {:display_name => "Resident Assistant"}}
     ability.can [:read, :create, :shift_log, :update, :update_shift_times], Shift, :staff_id => staff.id
     ability.can :update, Building
-	ability.can :manage, NotificationPreference, :staff_id => staff.id
-	ability.can :manage, Task
-	ability.can :index, [Building, Area, Task, RelationshipToReport]
-	ability.can :index, Staff, :organizations => { :id => self.id }
-	ability.can [:update, :show], Staff, { :access_levels => {:name => "ResidentAssistant"}, :organizations => { :id => self.id } }
-	ability.can :assign, Area
-	ability.can [ :index, :search], Report, {:type => MY_REPORT_TYPES }
-	ability.can [:select], ReportType, { :name => MY_REPORT_TYPES }
+    ability.can :manage, NotificationPreference, :staff_id => staff.id
+    ability.can :manage, Task
+    ability.can :index, [Building, Area, Task, RelationshipToReport]
+    ability.can :index, Staff, :organizations => { :id => self.id }
+    ability.can [:update, :show], Staff, { :access_levels => {:name => "ResidentAssistant"}, :organizations => { :id => self.id } }
+    ability.can :assign, Area
+    ability.can [ :index, :search], Report, {:type => MY_REPORT_TYPES }
+    ability.can [:select], ReportType, { :name => MY_REPORT_TYPES }
   end
   
   def staff(ability, staff)
@@ -120,22 +124,22 @@ class ResidenceLifeOrganization < Organization
   def resident_assistant(ability, staff)
     puts ( "*********Apply abilities to resident assistant")
     ability.can [:index, :search], Participant
-	ability.can [:search], MY_REPORTS
-	ability.can [:create], Report, { :type => MY_REPORT_TYPES }
+    ability.can [:search], MY_REPORTS
+    ability.can [:create], Report, { :type => MY_REPORT_TYPES }
     ability.can [:add_participant, :create_participant_and_add_to_report, :remove_participant], Report, {  :submitted  => false, :staff_id => staff.id }
-	ability.can [:index, :search], Report, { :staff_id => staff.id, :type => MaintenanceReport.to_s }
-	ability.can [:index, :update], Report, { :staff_id => staff.id, :submitted  => false, :type => MY_REPORT_TYPES }
-	# Establish authority to select reports from menu
-	ability.can [:select], ReportType, { :name => MY_REPORT_TYPES }
-	ability.can [:show], [MaintenanceReport, Note], {:staff_id => staff.id }
-	ability.can [:show], IncidentReport, { :staff_id => staff.id, :submitted  => false }
-	ability.can :do, Shift, :time_out => nil
+    ability.can [:index, :search], Report, { :staff_id => staff.id, :type => MaintenanceReport.to_s }
+    ability.can [:index, :update], Report, { :staff_id => staff.id, :submitted  => false, :type => MY_REPORT_TYPES }
+    # Establish authority to select reports from menu
+    ability.can [:select], ReportType, { :name => MY_REPORT_TYPES }
+    ability.can [:show], [MaintenanceReport, Note], {:staff_id => staff.id }
+    ability.can [:show], IncidentReport, { :staff_id => staff.id, :submitted  => false }
+    ability.can :do, Shift, :time_out => nil
     ability.can :do, Round, :end_time => nil
-	ability.can :do, TaskAssignment
+    ability.can :do, TaskAssignment
     ability.can [:shift_log, :read], Shift, { :staff_id => staff.id }
-	# staff-related
-	ability.can :index, Staff, :organizations => { :id => self.id }
-	ability.can [:update, :show], Staff, :id => staff.id
+    # staff-related
+    ability.can :index, Staff, :organizations => { :id => self.id }
+    ability.can [:update, :show], Staff, :id => staff.id
 	
   end
 			
