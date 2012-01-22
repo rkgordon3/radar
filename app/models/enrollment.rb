bd2c371b036f2c62192c08bf3123dd7bb32b8512
Rob Gordon <suffolksoft@gmail.com>
2012-01-22 12:32:24 -0600

    added commentary

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,6 +1,8 @@
 class Enrollment < ActiveRecord::Base
 	
     def Enrollment.for (students)
+	# For each student, find courses, build an array of course_id arrays 
+	# then use inject to do an intersection on the course_id arrays
 		Course.find_all_by_id(students.collect { |s| Enrollment.where(:student_id=>s.student_id).all.collect { |e| e.course_id } }.inject { |a,b| a & b } )
     end     
 end
