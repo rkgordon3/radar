251e13a967d85398b180d925e4197b14fc98b791
Rob Gordon <suffolksoft@gmail.com>
2012-01-22 12:28:21 -0600

    used inject to do course_id intersection

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,17 +1,7 @@
 class Enrollment < ActiveRecord::Base
 	
     def Enrollment.for (students)
-		enrollments = []
-        students.each do |s|
-			enrollments << Enrollment.where(:student_id => s.student_id).all.collect { |e| e.course_id }
-        end
-
-		sz = enrollments.size-1
-		while sz > 0
-			enrollments[0] = enrollments[0] & enrollments[sz]
-			sz = sz -1
-		end
-        Course.find_all_by_id(enrollments[0])
+		Course.find_all_by_id(students.collect { |s| Enrollment.where(:student_id=>s.student_id).all.collect { |e| e.course_id } }.inject { |a,b| a & b } )
     end     
 end
 
