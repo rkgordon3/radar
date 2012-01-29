70b674faaedc45816bb40911b20f18c767f86959
Rob Gordon <suffolksoft@gmail.com>
2012-01-29 10:12:01 -0600

    remove obsolete code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,6 @@ class HomeController < ApplicationController
     if staff_signed_in?
       @unsubmitted = Report.accessible_by(current_ability).where(:submitted => false, :staff_id => current_staff.id)
       @unsubmitted = @unsubmitted.order("reports.#{Report.default_sort_field} DESC")
-      #@report_type = ReportType.find_by_name("IncidentReport")
       @recent = Report.accessible_by(current_ability).where(:created_at => (current_staff.last_sign_in_at)..(Time.now), :submitted => true)
       @recent = @recent.order("reports.#{Report.default_sort_field} DESC")
     end
