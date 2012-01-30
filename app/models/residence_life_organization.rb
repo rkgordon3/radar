fd2b87d565cb3a9eeac44c4669febe3322134e2d
Rob Gordon <suffolksoft@gmail.com>
2012-01-30 15:12:10 -0600

    add pdf, show for IR to Campus Safety

--- app/models/residence_life_organization.rb
+++ app/models/residence_life_organization.rb
@@ -84,9 +84,9 @@ class ResidenceLifeOrganization < Organization
   
   def campus_safety(ability, staff)
     ability_base [IncidentReport, Note], ability, staff
-	
+
     ability.can [:view_contact_info], Participant
-   
+    ability.can [:pdf, :show], IncidentReport, { :staff_id => staff.id }
     ability.can :update, NotificationPreference, :staff_id => staff.id
 
   end
