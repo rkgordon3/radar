8a2e637aef30ce773d811c40a33f7feeabdc19be
Rob Gordon <suffolksoft@gmail.com>
2012-02-23 19:02:20 -0600

    PT 25424113 : fixed admin asst abilities

--- app/models/academic_skills_center_organization.rb
+++ app/models/academic_skills_center_organization.rb
@@ -17,7 +17,7 @@ class AcademicSkillsCenterOrganization < Organization
   private
   
   def administrator_base reports, ability, staff
-    ability.can [ :view_contact_info, :view_contact_history], Participant
+    ability.can [ :view_contact_info, :view_contact_history, :show], Participant
 	reports.each { |r| ability.can [:show,:index, :pdf, :forward, :update], Report, { :type => r.name } } 
 	# required to show contact history
 	ability.can :read, ReportParticipantRelationship, { :report => {:type => MY_REPORT_TYPES} }
@@ -29,7 +29,7 @@ class AcademicSkillsCenterOrganization < Organization
 	ability_base MY_REPORTS, ability, staff
 	administrator_base MY_REPORTS, ability, staff
 	
-  	ability.can :assign, AccessLevel, :name => ["Administrator", "AdministrativeAssistant", "Supervisor", "Staff"]
+  	ability.can :assign, AccessLevel, :name => ["AdministrativeAssistant", "Supervisor", "Staff"]
 
     # Limit access to those reports in this organization
     ability.can [:create, :read, :show, :index, :update, :search, :pdf], Report, {:type => MY_REPORT_TYPES} 
@@ -45,12 +45,13 @@ class AcademicSkillsCenterOrganization < Organization
   def administrative_assistant(ability, staff)
 	ability_base MY_REPORTS, ability, staff
 	
-    ability.can [ :view_contact_info, :view_contact_history], Participant
+    ability.can [ :view_contact_info, :view_contact_history, :show], Participant
 
     #ability.can [:pdf, :show, :forward], Report, { :type => MY_REPORT_TYPES }
     MY_REPORT_TYPES.each { |r| ability.can [:show, :index, :pdf, :forward], Report, { :type => r } }  
 	
 	ability.can [:update, :show], Staff, { :access_levels => {:name => [ "Supervisor", "Staff" ]} , :organizations => { :id => self.id } }
+	ability.can :assign, Organization, :id => self.id
 
     ability.can :read, ReportParticipantRelationship, { :report => {:type => MY_REPORT_TYPES} }
   end
@@ -58,10 +59,11 @@ class AcademicSkillsCenterOrganization < Organization
   def supervisor(ability, staff)
 	ability_base MY_REPORTS, ability, staff
 	
-	ability.can [:view_contact_info, :view_contact_history], Participant
+	ability.can [:view_contact_info, :view_contact_history, :show], Participant
 	# Staff
     ability.can [:update, :show], Staff, { :access_levels => {:name =>  "Staff" }, :organizations => { :id => self.id } }
-    
+    ability.can :assign, Organization, :id => self.id
+
     MY_REPORT_TYPES.each { |r| ability.can [:show, :index, :pdf, :forward], Report, { :type => r } }  
 
     ability.can :read, ReportParticipantRelationship, { :report => {:type => MY_REPORT_TYPES} }
