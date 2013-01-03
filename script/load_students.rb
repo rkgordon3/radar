5dfad835075fdaabc34bebd71ecdbe97610e84d1
Rob Gordon <suffolksoft@gmail.com>
2013-01-03 12:40:00 -0600

    fixed missing paren in scripty

--- script/load_students.rb
+++ script/load_students.rb
@@ -6,7 +6,7 @@ end
 require File.join(File.dirname(__FILE__), "..", "config", "boot")
 require File.join(File.dirname(__FILE__), "..", "config", "environment")
 
-lines = ImportsHelper.parse_csv_file(File.expand_path(ARGV[0])
+lines = ImportsHelper.parse_csv_file(File.expand_path(ARGV[0]))
 lines.shift
 print "Import #{lines.size} records"
 load_results = ImportsHelper.load_students(lines)
