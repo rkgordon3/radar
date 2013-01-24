b579bbd0cd01041fa0d8c7a944e9e2c517528345
Rob Gordon <suffolksoft@gmail.com>
2013-01-24 16:48:38 -0600

    added confirm dialog when submitting MR

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,18 @@ def ApplicationHelper.unknown_date
   Date.civil(0,1,1)
 end
 
-
 UNKNOWN_IMAGE = "person-silhouette.jpg"
 
 def ApplicationHelper.unknown_image
  UNKNOWN_IMAGE
+end
+
+def maintenance_report_warning_text 
+   %Q[Before submitting a Maintenance Request involving a fire alarm problem, a\nheating or hot water issue, or a security/entrance issue, please radio Unit 1.
+
+DO NOT submit a Maintenance Request involving computer, printer, or network\nissues. Instead, please call the Helpdesk at x7800 or email helpdesk@smumn.edu.
+
+Press Okay to submit a Maintenance Request, otherwise press Cancel.].html_safe
 end
 
 end
