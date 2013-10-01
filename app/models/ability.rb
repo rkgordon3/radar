e8fd4ec614fc6798bf4597d69828d1efad18599f
Rob Gordon <suffolksoft@gmail.com>
2013-10-01 10:07:57 -0500

    Created alias for new_staff & create_staff actions on StaffController
    New alias allows system administrator to add new staff users.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,8 +9,9 @@ class Ability
     alias_action :call_log, :duty_log, :to => :shift_log
     alias_action :autocomplete_participant_full_name, :sort_search_results, :search_results, :to => :search
     alias_action :forward_as_mail, :to => :forward
+    alias_action :new_staff, :create_staff, :to => :create
     alias_action :new, :activate, :new_with_participants,  :to => :create # I tried to include all actions needed throughout the report creation process -Lyric
-	alias_action :update_duration, :update_common_duration, :update_reason, :new_with_participants,
+    alias_action :update_duration, :update_common_duration, :update_reason, :new_with_participants,
       :update_common_reasons, :render_set_reason, :render_common_reasons_update, :update_common_annotation,
       :update_annotation, :remove_participant, :create_participant_and_add_to_report, :add_participant, :to => :modify_live
 
