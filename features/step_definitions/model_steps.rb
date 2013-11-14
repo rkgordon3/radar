d26d8fa0abfe8699708fd94dde7f952e21588ed0
Dylan Ethen <dlethe10@smumn.edu>
2013-11-14 15:46:39 -0600

    Updating RA Student Search by Name to ensure the student exists to increase test quality.

--- features/step_definitions/model_steps.rb
+++ features/step_definitions/model_steps.rb
@@ -5,3 +5,11 @@ end
 Given(/^the (.*?) named (.*?) exists$/) do |model, name|
   FactoryGirl.create(model.downcase.to_sym, :name => name)
 end
+
+Given(/^the (.*?) named (.*?) should exist$/) do |model, name|
+  model.capitalize.constantize.find_by_name(name)
+end
+
+Given(/^the student with name (.*?) should exist$/) do |name|
+  Student.find_by_full_name(name)
+end
