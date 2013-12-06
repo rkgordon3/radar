a2ced7a0b067047d55b69763139681c8da1fd51e
gabegrundtner <gbgrun10@smumn.edu>
2013-12-06 09:56:38 -0800

    pushing changes to js files for adding participants to reports'

--- features/support/driver.rb
+++ features/support/driver.rb
@@ -1,4 +1,4 @@
-require 'capybara/poltergeist'
+#require 'capybara/poltergeist'
 #Capybara.javascript_driver = :poltergeist
 Capybara.javascript_driver = :selenium
 #Capybara.javascript_driver = :webkit
