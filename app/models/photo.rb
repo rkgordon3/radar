82e4caaddcbd2e598f48b4cf741484861e645198
David <djcorp10@smumn.edu>
2013-12-15 15:17:04 -0600

    working on upload

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,15 +1,22 @@
-class Photo < ActiveRecord::Base
+require 'httpclient/include_client'
+class Photo  < ActiveRecord::Base.extend HTTPClient::IncludeClient
 	# belongs_to				:report
   	attr_accessible :description, :g3_id, :id, :owner_id, :title
 
-  	def self.upload(path)
+  	def self.upload(path, name)
+      include_http_client
+      
   		clnt = HTTPClient.new
-  		uri = "http://140.190.71.125/gallery3/"
+  		uri = URI.parse("http://140.190.71.125/gallery3/index.php/rest/create/photo/#{name}")
   		File.open(path) do |file|
-        puts file
   			body = { 'upload' => file, 'user' => 'nahi' }
-  			res = clnt.post(uri, body, { 'Content-Type' => 'image/jpeg'})
+  			res = clnt.post(uri, body)
+        puts '*****************'
+        puts res.inspect
+        puts '*****************'
 		  end
 		puts "*File Uploaded*"
+    # index.php/rest/create/photo/image1
+    # file <= multipart
   	end
 end
